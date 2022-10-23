7cff8ffef86380fcd7ca521d19b8be621b4e0cb5
Dustin Moris Gorski <dustinmoris@gmail.com>
2022-10-23 17:01:11 +0100

    Improved mobile views

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,18 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ["./cmd/**/*.{html,js,md}"],
   theme: {
+    screens: {
+      'xs': '400px',
+      ...defaultTheme.screens,
+    },
     fontFamily: {
       'sans': ['Inter', ...defaultTheme.fontFamily.sans],
       'serif': [...defaultTheme.fontFamily.serif],
       'mono': ['Inconsolata', ...defaultTheme.fontFamily.mono],
       'nunito': ['Nunito', ...defaultTheme.fontFamily.sans]
-  },
-    extend: {},
+    },
+    extend: {
+    },
   },
   plugins: [],
 }
